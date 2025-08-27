81789473ade4b1c2702ff9a5a20eed0cd302615f
Mithlesh Kumar <mithleshji2021@gmail.com>
2025-08-27 12:37:17 +0530

    toast add in protected route

--- src/components/common/Protected.jsx
+++ src/components/common/Protected.jsx
@@ -1,10 +1,12 @@
 import { useUser } from '@clerk/clerk-react'
 import { Navigate, Outlet } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const Protected = () => {
     const {isSignedIn } = useUser();
     
     if(!isSignedIn){
+        toast.error("Login First")
         return <Navigate to='/' />
     }
 
